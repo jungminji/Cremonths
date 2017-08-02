8fc04d583c865a64351abd881f63bcdda9ba18f9
jungminji <fdev.jm@gmail.com>
2017-08-02 11:22:13 +0900

    Minor fix

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     Controls
     hr
     Result
+    h5.footer-text Designed by Erica Niu
+    h5.footer-text Powered by Vue 2
+    
 </template>
 
 <script>
@@ -19,4 +22,11 @@ export default {
 </script>
 
 <style lang="sass">
+body
+  font-family: 'Open Sans', sans-serif
+  
+.footer-text
+  color: grey
+  font-size: 12px
+  font-weight: lighter
 </style>
